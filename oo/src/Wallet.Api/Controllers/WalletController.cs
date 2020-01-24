e30fc65e538fa22c66f9888c0d9f91e84e77e9fb
Guilherme Porto <guiportooo@gmail.com>
2020-01-24 08:16:16 -0300

    add oo sln

---
+++ oo/src/Wallet.Api/Controllers/WalletController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wallet.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalletController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok();
+    }
+}
